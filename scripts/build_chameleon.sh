5657a3b68fd006dca17c8e2d79d18cd24cf0f080
Simon Weis <laguna.ws@gmail.com>
2024-11-08 17:17:21 +0100

    Script for building chameleon

---
+++ scripts/build_chameleon.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+WORKSPACE_DIR=`pwd`
+BUILD_FOLDER="$WORKSPACE_DIR/build/chameleon"
+
+echo "$WORKSPACE_DIR"
+
+# copy files
+
+cd $WORKSPACE_DIR/chameleon/manual
+mkdir -p $BUILD_FOLDER
+cp -r assets $BUILD_FOLDER/
+cp *.md $BUILD_FOLDER/
+cp $WORKSPAC_DIR/common/*.md $BUILD_FOLDER/
+cp $WORKSPAC_DIR/common/style_dark.css $BUILD_FOLDER/
+cp $WORKSPAC_DIR/common/style_print.css $BUILD_FOLDER/
+cp -r $WORKSPAC_DIR/common/assets $BUILD_FOLDER/
+tree $BUILD_FOLDER/
+
+
+cd $BUILD_FOLDER/
+# build fancy version
+pandoc *.md -o chameleon_manual.pdf --filter pandoc-secnos --pdf-engine=weasyprint --css style_dark.css --metadata title="Chameleon" -V "title:"
+# build print version
+pandoc *.md -o chameleon_manual_print.pdf --filter pandoc-secnos --pdf-engine=weasyprint --css style_print.css --metadata title="Chameleon" -V "title:"
