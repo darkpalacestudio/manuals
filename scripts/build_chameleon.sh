3d07a025d4749232ada70634f019f0d2f903adbc
Kenneth van den Berg <kyniverse@gmail.com>
2025-05-03 11:10:25 +0200

    Made paths in scripts )S independent

--- scripts/build_chameleon.sh
+++ scripts/build_chameleon.sh
@@ -7,18 +7,18 @@ echo "$WORKSPACE_DIR"
 
 # copy files
 
-cd $WORKSPACE_DIR/chameleon/
-mkdir -p $BUILD_FOLDER
-cp -r assets $BUILD_FOLDER/
-cp *.md $BUILD_FOLDER/
-cp $WORKSPACE_DIR/common/*.md $BUILD_FOLDER/
-cp $WORKSPACE_DIR/common/style_dark.css $BUILD_FOLDER/
-cp $WORKSPACE_DIR/common/style_print.css $BUILD_FOLDER/
-cp -r $WORKSPACE_DIR/common/assets $BUILD_FOLDER/
-tree $BUILD_FOLDER/
+cd "$WORKSPACE_DIR/chameleon/"
+mkdir -p "$BUILD_FOLDER"
+cp -r assets "$BUILD_FOLDER/"
+cp *.md "$BUILD_FOLDER/"
+cp "$WORKSPACE_DIR/common/"*.md "$BUILD_FOLDER/"
+cp "$WORKSPACE_DIR/common/style_dark.css" "$BUILD_FOLDER/"
+cp "$WORKSPACE_DIR/common/style_print.css" "$BUILD_FOLDER/"
+cp -r "$WORKSPACE_DIR/common/assets" "$BUILD_FOLDER/"
+tree "$BUILD_FOLDER/"
 
 
-cd $BUILD_FOLDER/
+cd "$BUILD_FOLDER/"
 # build fancy version
 pandoc *.md -o chameleon_manual.pdf --filter pandoc-secnos --pdf-engine=weasyprint --css style_dark.css --metadata title="Chameleon" -V "title:"
 # build print version
